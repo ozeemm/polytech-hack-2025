8cde7cf252937868078acfc502566132ced54bae
miffteen <sabitovmihail15@gmail.com>
2025-04-29 01:58:56 +0800

    PostCommit

--- server.py
+++ server.py
@@ -1,19 +1,35 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import sqlite3
 import json
 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
     conn = sqlite3.connect('Hack.db')
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM DataWithClean Limit 5")
+    cursor.execute("SELECT * FROM DataWithClean WHERE uuid = 27648")
     rows = cursor.fetchall()
     conn.close()
 
     result = [dict(row) for row in rows]
 
     return jsonify(result)
+
+
+@app.route("/", methods=["POST"])
+def index1():
+    data = request.json
+    conn = sqlite3.connect('Hack.db')
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM DataWithClean WHERE route = {data["RouteID"]} LIMIT 1000")
+    rows = cursor.fetchall()
+    conn.close()
+
+    result = [dict(row) for row in rows]
+
+    return jsonify(result)
+
