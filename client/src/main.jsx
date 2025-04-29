24662076d75e1312213d5233eb2b20a9ec5833bc
ozeemm <77226278+ozeemm@users.noreply.github.com>
2025-04-30 02:16:45 +0800

    Added open animation for map filters

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-import 'bootstrap/dist/css/bootstrap.css';
-
-import '@bopen/leaflet-area-selection/dist/style.css';
-
-//import 'select2/src/scss/core'
-//import 'select2-bootstrap-5-theme'
-// import "bootstrap/dist/js/bootstrap"
-
 import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap-icons/font/bootstrap-icons.min.css"
 import 'bootstrap/dist/js/bootstrap'
 
 createRoot(document.getElementById('root')).render(
