e4be3f470df0cd5a481f0cbc8ef52ce58849de01
Ana <dns80791@gmail.com>
2025-04-30 03:33:42 +0800

    added three mode on city roads

---
+++ geojson_func.py
@@ -0,0 +1,216 @@
+import pandas as pd
+import matplotlib.cm as cm
+import matplotlib.colors as mcolors
+
+from datetime import timedelta
+import random
+
+from geopy.distance import geodesic
+
+import geopandas as gpd
+import pandas as pd
+from shapely.geometry import MultiLineString
+from shapely.ops import nearest_points
+
+def generate_geojson(data, mode):
+    """
+    Генерирует GeoJSON с отрезками маршрутов.
+    
+    Параметры:
+    - df: DataFrame с полями ['signal_time', 'uuid', 'route', 'lat', 'lon', 'speed']
+    - mode: 'route' | 'all' | 'uuid' — режим работы
+    - route_id: номер маршрута (str/int) для 'route' и 'uuid' режимов
+    - uuid: ID ТС для режима 'uuid'
+    - output_file: путь к файлу вывода
+    - time_limit_min: макс. интервал между точками
+    - distance_threshold_km: мин. расстояние между точками (для режима 'all')
+    - colormap_name: имя цветовой карты
+    """
+    
+    data = pd.DataFrame(data)
+
+    print(data.columns)
+
+    data['signal_time'] = pd.to_datetime(data['signal_time'])
+
+    geojson_data = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+
+    min_signal_time_delta = 1
+    distance_threshold_km = 0.1
+
+    if (mode=='speed'):
+        colormap = cm.RdYlGn 
+    else:
+        colormap = cm.Set1
+
+    if mode == 'speed':
+        speed_min = data['speed'].min()
+        speed_max = data['speed'].max()
+        norm = mcolors.Normalize(vmin=speed_min, vmax=speed_max)
+
+        geojson_data["speed_min"] = float(speed_min)
+        geojson_data["speed_max"] = float(speed_max)
+
+        for i in range(len(data) - 1):
+            p1, p2 = data.iloc[i], data.iloc[i + 1]
+
+            delta = p2['signal_time'] - p1['signal_time']
+            distance = geodesic((p1['lat'], p1['lon']), (p2['lat'], p2['lon'])).kilometers
+
+            if (p1['uuid'] == p2['uuid']) and (delta < timedelta(minutes=min_signal_time_delta)) and (distance > distance_threshold_km):
+                speed = p1['speed']
+                color = mcolors.to_hex(colormap(norm(speed)))
+
+                geojson_data["features"].append({
+                    "type": "Feature",
+                    "properties": {
+                        "speed_from": float(p1['speed']),
+                        "speed_to": float(p2['speed']),
+                        "color": color,
+                        "signal_time_from": p1['signal_time'].isoformat(),
+                        "signal_time_to": p2['signal_time'].isoformat(),
+                        "uuid_from": int(p1['uuid']),
+                        "uuid_to": int(p2['uuid']),
+                        "route": str(p1['route'])
+                    },
+                    "geometry": {
+                        "type": "LineString",
+                        "coordinates": [[p1['lon'], p1['lat']], [p2['lon'], p2['lat']]]
+                    }
+                })
+
+    elif mode == 'routes':
+        for route in data['route'].unique():
+            for vtype in data[data['route'] == route]['vehicle_type'].unique():
+                points = data[(data['route'] == route) & (data['vehicle_type'] == vtype)].sort_values('signal_time')
+                color = mcolors.to_hex(colormap(random.random()))
+
+                for i in range(len(points) - 1):
+                    p1, p2 = points.iloc[i], points.iloc[i + 1]
+                    delta = p2['signal_time'] - p1['signal_time']
+                    distance = geodesic((p1['lat'], p1['lon']), (p2['lat'], p2['lon'])).kilometers
+
+                    if (p1['uuid'] == p2['uuid']) and (delta < timedelta(minutes=min_signal_time_delta)) and (distance > distance_threshold_km):
+                        geojson_data["features"].append({
+                            "type": "Feature",
+                            "properties": {
+                                "speed_from": float(p1['speed']),
+                                "speed_to": float(p2['speed']),
+                                "color": color,
+                                "signal_time_from": p1['signal_time'].isoformat(),
+                                "signal_time_to": p2['signal_time'].isoformat(),
+                                "uuid_from": int(p1["uuid"]),
+                                "uuid_to": int(p2["uuid"]),
+                                "route": str(p1['route'])
+                            },
+                            "geometry": {
+                                "type": "LineString",
+                                "coordinates": [[p1['lon'], p1['lat']], [p2['lon'], p2['lat']]]
+                            }
+                        })
+
+    elif mode == 'transports':
+        for uuid in data['uuid'].unique():
+            points = data[data['uuid'] == uuid].sort_values('signal_time')
+            color = mcolors.to_hex(colormap(random.random()))
+        
+            for i in range(len(points) - 1):
+                p1, p2 = points.iloc[i], points.iloc[i + 1]
+                delta = p2['signal_time'] - p1['signal_time']
+
+                if (delta < timedelta(minutes=min_signal_time_delta)):
+                    geojson_data["features"].append({
+                        "type": "Feature",
+                        "properties": {
+                            "speed_from": float(p1['speed']),
+                            "speed_to": float(p2['speed']),
+                            "color": color,
+                            "signal_time_from": p1['signal_time'].isoformat(),
+                            "signal_time_to": p2['signal_time'].isoformat(),
+                            "uuid_to": int(p1["uuid"]),
+                            "uuid_from": int(p2["uuid"]),
+                            "route": str(p1['route'])
+                        },
+                        "geometry": {
+                            "type": "LineString",
+                            "coordinates": [[p1['lon'], p1['lat']], [p2['lon'], p2['lat']]]
+                        }
+                    })
+
+    return geojson_data
+
+
+
+
+def transfer_nearest_properties(geojson_lines):
+    def to_multiline(geom):
+        if geom.geom_type == 'LineString':
+            return MultiLineString([geom])
+        elif geom.geom_type == 'MultiLineString':
+            return geom
+        else:
+            return None
+
+    # Загрузка GeoJSON
+    gdf_base = gpd.read_file('Граф Иркутск_link_geojson.geojson')     # Граф УДС
+    gdf_target =  gpd.GeoDataFrame.from_features(geojson_lines['features'])
+
+# 3. Назначаешь геометрию явно (если вдруг не подхватилось)
+    gdf_target.set_geometry('geometry', inplace=True)
+
+
+# 3. Устанавливаем CRS (например, EPSG:4326)
+    gdf_target.set_crs('EPSG:4326', inplace=True)
+    # Приведение геометрий к MultiLineString
+    gdf_target['geometry'] = gdf_target['geometry'].apply(to_multiline)
+
+    # Установка CRS
+    gdf_base = gdf_base.to_crs(epsg=4326)
+    gdf_target = gdf_target.to_crs(epsg=4326)
+
+    # Пространственный индекс
+    gdf_target_sindex = gdf_target.sindex
+
+    # Поиск ближайших свойств
+    def get_nearest_properties(geom):
+        bounds = geom.bounds
+        possible_matches_index = list(gdf_target_sindex.intersection(bounds))
+        possible_matches = gdf_target.iloc[possible_matches_index]
+
+        nearest_geom = None
+        min_distance = float('inf')
+
+        for _, row in possible_matches.iterrows():
+            dist = geom.distance(row['geometry'])
+            if dist < min_distance:
+                min_distance = dist
+                nearest_geom = row
+
+        if nearest_geom is not None:
+            return {
+                "speed_from": float(nearest_geom['speed_from']),
+                "speed_to": float(nearest_geom['speed_to']),
+                "color": nearest_geom['color'],
+                "signal_time_from": nearest_geom['signal_time_from'],
+                "signal_time_to": nearest_geom['signal_time_to'],
+                "uuid_from": int(nearest_geom['uuid_from']),
+                "uuid_to": int(nearest_geom['uuid_to'])
+            }
+        return {}
+
+    # Применяем
+    properties = gdf_base['geometry'].apply(get_nearest_properties)
+    properties_df = pd.json_normalize(properties)
+
+    gdf_result = gdf_base.join(properties_df)
+    gdf_result = gpd.GeoDataFrame(gdf_result, geometry='geometry')
+
+    # Удаляем строки, где 'color' или другие важные свойства отсутствуют (незакрашенные линии)
+    gdf_result = gdf_result[gdf_result['color'].notna()]
+
+    geojson = gdf_result.to_json()
+
+    return geojson
--- server.py
+++ server.py
@@ -1,10 +1,8 @@
-import pandas as pd
-import matplotlib.cm as cm
-import matplotlib.colors as mcolors
-
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import sqlite3
+
+from geojson_func import *
 
 app = Flask(__name__)
 CORS(app)
@@ -14,49 +12,6 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-def get_speed_colored_route_geojson(data):
-    points = pd.DataFrame(data)
-
-    speed_min = points['speed'].min()
-    speed_max = points['speed'].max()
-    norm = mcolors.Normalize(vmin=speed_min, vmax=speed_max)
-    colormap = cm.RdYlGn
-    
-    geojson_data = {
-        "type": "FeatureCollection",
-        "speed_min": speed_min,
-        "speed_max": speed_max,
-        "features": []
-    }
-
-    # Для каждого отрезка (между точками) добавляем его в GeoJSON
-    for i in range(len(points) - 1):
-        point1 = points.iloc[i]
-        point2 = points.iloc[i + 1]
-
-        # Цвет по скорости
-        speed = point1['speed']  # Берем скорость из первой точки отрезка
-        rgba = colormap(norm(speed))  # Получаем RGBA цвет
-        hex_color = mcolors.to_hex(rgba)  # Преобразуем в HEX
-
-        # Создаём GeoJSON объект для отрезка
-        feature = {
-            "type": "Feature",
-            "properties": {
-                "speed": speed,
-                "color": hex_color
-            },
-            "geometry": {
-                "type": "LineString",
-                "coordinates": [
-                    [point1['lon'], point1['lat']],
-                    [point2['lon'], point2['lat']]
-                ]
-            }
-        }
-        geojson_data["features"].append(feature)
-
-    return geojson_data
 
 
 @app.route("/api/getTransportFilters", methods=["GET"])
@@ -129,7 +84,7 @@ def ReturnWithFilters():
         cursor = conn.cursor()
         cursor.execute(f"""Select strftime('%m', signal_time) as month, 
                             strftime('%Y', signal_time) as year, time(signal_time) as time, 
-                            uuid, lat, lon, vehicle_type, route, speed, direction 
+                            uuid, lat, lon, vehicle_type, route, speed, direction, signal_time
                             FROM DataWithClean
                             WHERE time > ? AND time < ? 
                             AND month IN ({monthes}) AND year IN ({years})          
@@ -166,8 +121,7 @@ def ReturnWithFilters():
         AllTransport = busResult + tramResult + trolResult + miniBusResult
 
 
-        return get_speed_colored_route_geojson(AllTransport)
-    
+        return  transfer_nearest_properties(generate_geojson(AllTransport, data["colorMode"]))
 
 if __name__ == "__main__":
     app.run(debug=True)
